cf7f12a8cf20bffe95e6f041a25a881c52aa438b
jhumphreys-p <jhumphreysp@hotmail.com>
2021-03-22 16:09:46 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "your"];
+  let adj = ["great", "big", "small"];
+  let noun = ["joker", "head", "code"];
+  let extension = [".com", ".es", ".net"];
+
+  let domain = "";
+  let domains = [];
+
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var z = 0; z < adj.length; z++) {
+      for (var y = 0; y < noun.length; y++) {
+        for (var w = 0; w < extension.length; w++) {
+          domain = pronoun[i].concat(adj[z], noun[y], extension[w]);
+          domains.push(domain);
+        }
+      }
+    }
+  }
+
+  let body = document.querySelector("body");
+  let ulList = document.createElement("ul");
+
+  for (var x = 0; x < domains.length; x++) {
+    let listDomains = document.createElement("li");
+    let domains1By1 = document.createTextNode(domains[x]);
+    listDomains.appendChild(domains1By1);
+    ulList.appendChild(listDomains);
+    body.appendChild(ulList);
+  }
 };
